34c23793bc04fdc9260ac100136c6d90a7bac175
Wenzel Lowe <lowewenzel@gmail.com>
2019-02-03 01:13:37 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,159 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import withStyles from 'react-jss';
+import ArrowKeysReact from 'arrow-keys-react';
+import { CompactPicker } from 'react-color';
+import { addUrlProps, UrlQueryParamTypes } from 'react-url-query';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const urlPropsQueryConfig = {
+  numbers: { type: UrlQueryParamTypes.string, queryParam: 'numbers' },
+};
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexFlow: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    padding: 20
+  },
+  text: {
+    fontSize: '3em',
+    margin: 0,
+  },
+  textArea: {
+    width: '30%',
+    height: 200,
+    textAlign: 'center',
+    marginBottom: 10,
+    minWidth: 400,
+    fontSize: '2em'
+  },
+  display: {
+    fontFamily: 'PT Mono',
+    color: 'red',
+    fontSize: "10em",
+    margin: 0,
+    lineHeight: 0,
   }
 }
 
-export default App;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(ArrowKeysReact.events)
+    ArrowKeysReact.config({
+      left: () => {
+        console.log('left key detected.');
+      },
+      right: () => {
+        console.log('right key detected.');
+      },
+      up: () => {
+        console.log('up key detected.');
+      },
+      down: () => {
+        console.log('down key detected.');
+      }
+    });
+
+    this.state = {
+      nums: [],
+      textarea: "",
+      currentNum: null,
+      currentIndex: null,
+      background: 'white',
+      textColor: '#d33115'
+    }
+
+  }
+
+  handleBg = (color) => {
+    this.setState({ background: color.hex });
+  };
+  handleText = (color) => {
+    this.setState({ textColor: color.hex });
+  };
+
+  changeText = (e) => {
+    this.setState({
+      textarea: e.target.value
+    })
+  }
+
+  clickButton = () => {
+    const temp = this.state.textarea.replace(" ","").split('\n');
+    window.location = `/?numbers=${temp.join(',')}`
+  }
+
+  _handleKeyDown = (e) => {
+    if (e.keyCode == 39) {
+      if (! (this.state.currentIndex === this.state.nums.length - 1)) {
+        this.setState({
+            currentNum: this.state.nums[this.state.currentIndex + 1],
+            currentIndex: this.state.currentIndex + 1
+        })
+      }
+    } else if (e.keyCode == 37) {
+      if(! (this.state.currentIndex === 0)) {
+        this.setState({
+          currentNum: this.state.nums[this.state.currentIndex - 1],
+          currentIndex: this.state.currentIndex - 1
+        })
+      }
+    }
+  }
+
+  componentDidMount = () => {
+    if (this.props.numbers) {
+      const n = this.props.numbers.replace(" ", "").split(",");
+      this.setState({
+        nums: n,
+        currentNum: n[0],
+        currentIndex: 0
+      });
+    }
+    document.addEventListener("keydown", this._handleKeyDown);
+  }
+
+  componentWillUnmount = () => {
+    document.removeEventListener("keydown", this._handleKeyDown);
+  }
+
+  render() {
+    if (this.state.nums.length == 0) {
+      return (
+        <div className={this.props.classes.root} {...ArrowKeysReact.events}>
+          <h1 className={this.props.classes.text}>Separate Numbers By Line</h1>
+          <textarea className={this.props.classes.textArea} onChange={this.changeText} />
+          <div style={{display: 'flex', margin: 20}}>
+            <div style={{marginRight: 20}}>
+            <h1>Background</h1>
+            <CompactPicker  color={ this.state.background }
+          onChangeComplete={ this.handleBg }/>
+            </div>
+            <div>
+            <h1>Text</h1>
+            <CompactPicker  color={ this.state.textColor }
+          onChangeComplete={ this.handleText } />
+            </div>
+          </div>
+          <button style={{fontSize:'2em', borderRadius: 10, padding: 20}} onClick={this.clickButton}> Finish </button>
+          <p>Use Arrow Keys to Navigate. Ctrl +/- to zoom in and out.</p>
+
+        </div>
+      );
+    } else {
+      return (
+        <div className={this.props.classes.root} {...ArrowKeysReact.events} style={{background: this.state.background}}>
+          <h1 className={this.props.classes.display} style={{color: this.state.textColor}}>{this.state.currentNum}</h1>
+        </div>
+      )
+    }
+  }
+}
+
+export default (withStyles(styles)(addUrlProps({ urlPropsQueryConfig })(App)));
---
+++ src/comp/HymnInput.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class HymnInput extends Component {
+  render() {
+    return (
+      <textarea height="48"/>
+    );
+  }
+}
